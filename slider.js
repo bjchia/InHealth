9d1047a25cd644571c6968fa692a96d71dbb7471
Jay Rainton <jayrainton@gmail.com>
2017-10-07 12:45:56 -0700

    changed slider

---
+++ slider.js
@@ -0,0 +1,11 @@
+$("#slider").roundSlider({
+    radius: 60,
+    width: 14,
+    handleSize: "24,12",
+    handleShape: "square",
+    circleShape: "pie",
+    sliderType: "min-range",
+    startAngle: 40,
+    endAngle: 300,
+    value: 50
+});
