46718f8afa6d76ec88afc59dab53c9d5086360d2
bjchia <bjchia@ucdavis.edu>
2017-10-07 00:46:57 -0700

    commented out slider for now

--- notifications.js
+++ notifications.js
@@ -2,11 +2,11 @@ var options = {
 	type: "basic",
 	title: "My First Popup With Chrome!" , 
  	message: "This is pretty cool!" , 
- 	icon: "calhakcs.png"
+ 	icon: "calhacks.png"
 };
 
 chrome.notifications.create(options, callback);
 
 function callback() {
-
+	console.log('Popup done!');
 }
